33ae6371a4a13eb9c19b39dfb17fce09b60c8525
Russ Brown <50427328+russellnbrown@users.noreply.github.com>
2019-09-16 15:11:35 +1000

    Added csharp DB viewer

---
+++ cs_viewer/cs_viewer/CSViewForm.Designer.cs
@@ -0,0 +1,72 @@
+﻿namespace csview
+{
+    partial class CSViewForm
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.components = new System.ComponentModel.Container();
+            this.cslist = new System.Windows.Forms.ListView();
+            this.imglist = new System.Windows.Forms.ImageList(this.components);
+            this.SuspendLayout();
+            // 
+            // cslist
+            // 
+            this.cslist.Dock = System.Windows.Forms.DockStyle.Fill;
+            this.cslist.Location = new System.Drawing.Point(0, 0);
+            this.cslist.Margin = new System.Windows.Forms.Padding(1);
+            this.cslist.Name = "cslist";
+            this.cslist.Size = new System.Drawing.Size(800, 450);
+            this.cslist.TabIndex = 0;
+            this.cslist.UseCompatibleStateImageBehavior = false;
+            this.cslist.SelectedIndexChanged += new System.EventHandler(this.cslist_SelectedIndexChanged);
+            // 
+            // imglist
+            // 
+            this.imglist.ColorDepth = System.Windows.Forms.ColorDepth.Depth24Bit;
+            this.imglist.ImageSize = new System.Drawing.Size(32, 32);
+            this.imglist.TransparentColor = System.Drawing.Color.Transparent;
+            // 
+            // CSViewForm
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.cslist);
+            this.Name = "CSViewForm";
+            this.Text = "CSView";
+            this.Load += new System.EventHandler(this.CSViewForm_Load);
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.ListView cslist;
+        private System.Windows.Forms.ImageList imglist;
+    }
+}
+
---
+++ cs_viewer/cs_viewer/CSViewForm.cs
@@ -0,0 +1,115 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Drawing.Imaging;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace csview
+{
+    public partial class CSViewForm : Form
+    {
+        private  ImageConverter ic = new ImageConverter();
+        private string icPath;
+
+        public CSViewForm()
+        {
+            InitializeComponent();
+        }
+
+        private void cslist_SelectedIndexChanged(object sender, EventArgs e)
+        {
+  
+
+        }
+
+        private void CSViewForm_Load(object sender, EventArgs e)
+        {
+            Size bsize = new Size(16, 16);
+
+            cslist.LargeImageList = imglist;
+
+            ListViewItem lvi = new ListViewItem();
+            lvi.ImageIndex = 0;
+
+            cslist.View = View.LargeIcon;
+  
+
+            if (Environment.GetCommandLineArgs().Length != 2 )
+            {
+                MessageBox.Show("Specify path to img collection folder when starting");
+                return;
+            }
+            string fileName = Environment.GetCommandLineArgs()[1];
+            if (!Directory.Exists(fileName))
+            {
+                MessageBox.Show(fileName + " dosn't exist");
+                return;
+            }
+            fileName += "/images.bin";
+            using (BinaryReader reader = new BinaryReader(File.Open(fileName, FileMode.Open)))
+            {
+                while(reader.BaseStream.Position != reader.BaseStream.Length)
+                {
+                    Int64 crc = reader.ReadInt64();
+                    byte []ba = reader.ReadBytes(3 * 16 * 16);
+                    Bitmap bimg = new Bitmap(16, 16, PixelFormat.Format24bppRgb);
+                    Rectangle r = new Rectangle(0, 0, 16, 16);
+                    BitmapData bmpData = bimg.LockBits(r, ImageLockMode.WriteOnly, bimg.PixelFormat);
+                    int pad = bmpData.Stride - 3 * 16;
+                    unsafe
+                    {
+                        byte* ptr = (byte*)bmpData.Scan0;
+                        int wp = 0;
+                        int bx = 0;
+                        for(int y=0; y<16; y++)
+                        {
+                            for (int x = 0; x < 16; x++)
+                            {
+                                ptr[2] = ba[wp++];
+                                ptr[1] = ba[wp++];
+                                ptr[0] = ba[wp++];
+                                Console.WriteLine("B:" + bx + " : " + ptr[2] + ", " + ptr[1] + "," + ptr[0]);
+                                ptr += 3;
+                                bx++;
+                            }
+                            ptr += pad;
+                        }
+                        bimg.UnlockBits(bmpData);
+                    }
+  
+                    imglist.Images.Add((Image)bimg);
+                    lvi = new ListViewItem();
+                    lvi.ImageIndex = imglist.Images.Count-1;
+                    cslist.Items.Add(lvi);
+                    if (imglist.Images.Count > 1500)
+                        break;
+
+                }
+            }
+        }
+
+        public  Bitmap GetImageFromByteArray(byte[] byteArray)
+        {
+            Bitmap bm = (Bitmap)ic.ConvertFrom(byteArray);
+
+            if (bm != null && (bm.HorizontalResolution != (int)bm.HorizontalResolution ||
+                               bm.VerticalResolution != (int)bm.VerticalResolution))
+            {
+                // Correct a strange glitch that has been observed in the test program when converting 
+                //  from a PNG file image created by CopyImageToByteArray() - the dpi value "drifts" 
+                //  slightly away from the nominal integer value
+                bm.SetResolution((int)(bm.HorizontalResolution + 0.5f),
+                                 (int)(bm.VerticalResolution + 0.5f));
+            }
+
+            return bm;
+        }
+
+    }
+}
