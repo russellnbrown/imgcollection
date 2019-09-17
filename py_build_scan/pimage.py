86769b0a709cdc2354114908ec99c913593e542d
Russ Brown <50427328+russellnbrown@users.noreply.github.com>
2019-09-17 14:49:45 +1000

    drop

--- py_build_scan/pimage.py
+++ py_build_scan/pimage.py
@@ -8,9 +8,34 @@ import zlib
 import set
 from PIL import Image,ImageDraw,ImageFont
 from pathlib import Path,PurePosixPath
+import cfg
 
-log = logging.getLogger("fred")
 
+
+#
+# imgfileinfo - class used to hold useful information relating to an image file
+#
+class imgfileinfo(object):
+    ihash:int=0
+    dhash:int=0
+    tmb:bytearray
+    name:str
+    relpath:str
+
+    def __init__(self):
+        name=""
+        relpath=""
+        ihash=0
+        dhash=0
+
+    def __str__(self):
+        bl = len(self.tmb)
+        s = "imgfileinfo(ih=" + str(self.ihash) + ", dh=" + str(self.ihash) + ", tlen=" + str(bl) + ", na=" + self.name + ", relp=" + self.relpath + ")"
+        return s
+
+#
+# standardizePath - makes all paths confirm to a standard format with forward slashes, no tailing 
+#
 def standardizePath(p:str):
     p = p.replace("\\\\","/")
     p = p.replace("\\","/")
@@ -18,7 +43,11 @@ def standardizePath(p:str):
         p = p[0..len(p)-1]
     return p
 
+#
+# Set up logger
+#
 def setlogging():
+    log = logging.getLogger("pimage")
     handler = logging.StreamHandler()
     handler.formatter = logging.Formatter('%(asctime)s %(levelname)s : %(message)s')
     handler.level = logging.INFO
@@ -29,9 +58,14 @@ def setlogging():
     log.addHandler(handler)
     log.addHandler(fhandler)
 
+#
+# psplit - Splits a path into path & filename parts making sure path starts 
+# with a '/' and is relative to the top directoru
+#
 def psplit(p:Path):
     strp = p
-    stop = set.dtop
+    
+    stop = cfg.dtop
     rpos = str(os.path.relpath(strp,stop))
     rpos = standardizePath(rpos)
     if  rpos == "." : # catch first dir and remove '.'
@@ -40,46 +74,48 @@ def psplit(p:Path):
         rpos = "/" + rpos
     name = ""
     if not os.path.isdir(p) :
-        name = os.path.basename(p)
+        name = os.path.basename(p) # split off filename into name and use whats left in path
         rpos = rpos[0:len(rpos)-len(name)-1]
     else:
         name = ""
 
     return [ rpos,name ]
     
+#
+# procsetdir - called by file walker. creates a 'dirent' and adds to the dir list
+#
 def procsetdir(d:str):
     rpos = psplit(d)
     dhash = gethash(rpos[0])
-    de = set.dirent(dhash,rpos[0])
-    set.dlist.append(de)
+    de = cfg.dirent(dhash,rpos[0])
+    cfg.dlist.append(de)
     log.info("DIR: %s HASH: %d", rpos, dhash)
 
+#
+# getbhash - returns crc32 of a byte array ( the file content )
 def getbhash(key):
     hash = zlib.crc32(key)
     return hash
 
+#
+# finfo - takes an image file and calculates paths, hashes & thumbnail.
+# returns in a imgfileinfo class
+#
 def finfo(f:Path):
+    fi = imgfileinfo()
+    rpos = psplit(f)
+    fi.name = rpos[1]
+    fi.relpath = rpos[0]
+    fi.dhash = gethash(fi.relpath)
     ifile = f.open("rb") 
     by = ifile.read()
-    crc:int = getbhash(by)
+    fi.ihash = getbhash(by)
     image = Image.open(io.BytesIO(by))
     tn = image.resize([16,16])
     rgbtn = tn.convert('RGB')
-    iby = rgbtn.tobytes()
-    #print("len=", len(iby))
-    #bp = 0
-    #for row in range(1,16):
-    #    print("row=", row)
-    #    for c in range(1,16):
-    #        r = bp + 0
-    #        g = bp + 1
-    #        b = bp + 2
-    #        bp = bp + 3
-    #        print(" r=",iby[r]," g=",iby[g]," b=", iby[b])
-        
-
+    fi.tmb = rgbtn.tobytes()
     ifile.close()
-    return [ crc, iby ]
+    return fi
 
 def makeentries(f:Path):
     log.info("PFILE: %s", f)
@@ -92,20 +128,12 @@ def makeentries(f:Path):
 
 
 def procsetfile(f:Path):
-    log.info("PFILE: %s", f)
-    #rpos = psplit(f)
-    #ipos = finfo(f)
-    #dhash = gethash(rpos[0])
-    e = makeentries(f)
-    rpos = e[0]
-    ipos = e[1]
-    dhash = e[2]
-    fe = e[3]
-    ie = e[4]
-    #fe = set.filent(dhash,ipos[0],rpos[1])
-    #ie = set.imgent(ipos[0],ipos[1])
-    set.flist.append(fe)
-    set.imap[ipos[0]] = ie
+    fi = finfo(f)
+    log.info("Processing file %s", fi)
+    fe = cfg.filent(fi.dhash, fi.ihash, fi.name)
+    ie = cfg.imgent(fi.ihash, fi.tmb)
+    cfg.flist.append(fe)
+    cfg.imap[ie.ihash] = ie
 
 
 def file_walker(walk):
@@ -119,16 +147,17 @@ def file_walker(walk):
             procsetfile(item)
 
 def create(setl, files):
+ 
     setp = Path(setl) #standardizePath(setl)
     filesp = Path(files) #standardizePath(os.path.abspath(files))
-    set.dtop = Path(files)#.absolutePath() # standardizePath(os.path.abspath(files))
+    cfg.dtop = Path(files)#.absolutePath() # standardizePath(os.path.abspath(files))
 
     if not setp.is_dir() :
         log.warn("set dosnt exist, create")
         os.mkdir(setp);
 
-    if not set.dtop.is_dir() :
-        log.fatal("top dir %s dosn't exist, exit", set.dtop.str())
+    if not cfg.dtop.is_dir() :
+        log.fatal("top dir %s dosn't exist, exit", cfg.dtop.str())
         usage()
 
     if not filesp.is_dir() :
@@ -168,29 +197,32 @@ if ( len(sys.argv) < 3 ):
 if ( sys.argv[1] == "-s" and len(sys.argv) == 4 ):
     p = standardizePath(sys.argv[2])
     set.load(Path(sys.argv[2]))
+    set.printstate()
 
     f = standardizePath(sys.argv[3])
-    e = makeentries(f)
-    rpos = e[0]
-    ipos = e[1]
-    dhash = e[2]
-    fe = e[3]
-    ie = e[4]
+    fi = finfo(Path(f))
 
-    #set.search(standardizePath(sys.argv[3]))
-    set.search(ie)
-    for ci in set.results :
-        print(ci)
-    if ( len(set.results) == 0 ):
+    results = set.search(fi)
+    if ( len(results) == 0 ):
         print("No images found")
         exit(0)
 
-    ffile = set.findfile(set.results[0].img.crc)
-    if ( ffile != 0 ):
-        print("File: " , set.printpath(ffile))
+    for ci in results :
+        print("res:" , ci.img)
+        fi = set.findfile(ci.img.ihash)
+        if ( fi != 0 ):
+            print("Img " + str(ci.img.ihash) + ", cls=" + str(ci.close) + ", file=" + set.findfile(ci.img.ihash).fname)
+        else:
+            print("Img " + str(ci.img.ihash) + ", cls=" + str(ci.close) + ", file=?")
 
 elif ( sys.argv[1] == "-c" and len(sys.argv) == 4 ):
     create(sys.argv[2], sys.argv[3] )
+    log.info("Final state:")
+    #set.printstate()
+    #set.clear()
+    #set.load(Path(sys.argv[2]))
+    #log.info("Readback state:")
+    #set.printstate()
 
 else:
     usage();
--- py_build_scan/set.py
+++ py_build_scan/set.py
@@ -7,65 +7,12 @@ from pathlib import Path
 import logging
 from pathlib import Path,PurePosixPath
 from PIL import Image,ImageDraw,ImageFont
+import cfg
 
 log:logging.Logger
-tns:int = 16
-tnm:int = tns*tns*3
-dtop:Path
-#stop:str
 
-log = logging.getLogger("fred")
+log = logging.getLogger("pimage")
 
-
-class dirent(object):
-	dhash:int =0
-	dpath:str = ""
-	def __init__(self, _dh:int, _dp:str):
-		self.dhash=_dh
-		self.dpath=_dp
-
-	def __str__(self):
-		return "dilent(dh="+str(self.dhash)+", dp="+str(self.dpath)+")"
-
-
-class filent(object):
-	dhash:int=0
-	ihash:int=0
-	fname:str=""
-	def __init__(self, _dh:int, _ih:int, _fn:str):
-		self.dhash=_dh
-		self.ihash=_ih
-		self.fname=_fn
-
-	def __str__(self):
-		return "filent(dh="+str(self.dhash)+", ih="+str(self.ihash)+", fn="+str(self.fname)+")"
-
-class imgent(object):
-    ihash:int=0
-    tmb:bytearray
-    def __init__(self,  _ihash:int, _tn:bytearray):
-        self.ihash = _ihash
-        self.tmb = _tn
-
-    def __str__(self):
-        bl = len(self.tmb)
-        return "imgent(ih="+str(self.ihash)+", tlen=" + str(bl) +")"
-
-class compareditem(object):
-    img:imgent
-    close:float=0
-
-    def __init__(self,  _img:imgent, _close:float):
-        self.img = _img
-        self.close = _close
-
-    def __str__(self):
-        return "ci(im="+str(self.img.crc)+", cl="+str(self.close)+")"
-
-dlist:List[dirent] = []
-flist:List[filent] = []
-imap:Dict[int,imgent] = {}
-results:List[compareditem] = []
 
 def dbformat(p:Path):
     s:str = str(p)
@@ -79,7 +26,7 @@ def getcloseness(ci):
 def compare(simg, ximg):
     px = 0
     td = 0
-    while px < tns:
+    while px < cfg.tns:
         d = simg[px] - ximg[px]
         td += abs(d)
         px += 1
@@ -98,45 +45,27 @@ def printtn(iby):
             print(" r=",iby[r]," g=",iby[g]," b=", iby[b])
 
 def search(img):
-    #f = Path(fileToSearch)
-    #ifile = f.open("rb") 
-    #by = ifile.read()
-    #image = Image.open(io.BytesIO(by))
-    #tn = image.resize([16,16])
-    #rgbtn = tn.convert('RGB')
-    #iby = rgbtn.tobytes()
-    #printtn(iby)
-    #ifile.close()
-    for k,i in imap.items() :
-        cmp = compare(img, i.tmb)
-        ci = compareditem(i,cmp)
+    results:List[compareditem] = []
+    for k,i in cfg.imap.items() :
+        cmp = compare(img.tmb, i.tmb)
+        ci = cfg.compareditem(i,cmp)
         results.append(ci)
     results.sort(key=getcloseness) 
-
-def search2(fileToFind):
-    
-    oimg = cv2.imread(fileToFind)
-    rimg = cv2.resize(oimg,(tnm,tnm))
-    img = rimg.flatten()
-    for k,i in imap.items() :
-        cmp = compare(img, i.tmb)
-        ci = compareditem(i,cmp)
-        results.append(ci)
-    results.sort(key=getcloseness)
+    return results
 
 def findfile(crc:int):
-    for f in flist:
-        if ( f.fhash == crc ):
+    for f in cfg.flist:
+        if ( f.ihash == crc ):
             return f
     return 0
 
 def finddir(crc:int):
-    for d in dlist:
+    for d in cfg.dlist:
         if ( d.dhash == crc ):
             return d
     return 0
 
-def printpath(f:filent):
+def printpath(f:cfg.filent):
     d = finddir(f.dhash)
     if ( d ):
         return d.dpath + "/" + f.fname
@@ -146,44 +75,46 @@ def printpath(f:filent):
 def save(path:Path):
     dpath =  path / "dirs.txt"
     dirfile = dpath.open(mode="w+")
-    dirfile.write(dbformat(dtop.absolute())+"\n")
-    for  d in dlist :
+    dirfile.write(dbformat(cfg.dtop.absolute())+"\n")
+    for  d in cfg.dlist :
         dirfile.write(str(d.dhash)+","+d.dpath+"\n")
     dirfile.close()
 
     fpath =  path / "files.txt"
     ffile = fpath.open(mode="w+")
-    for  f in flist :
+    for  f in cfg.flist :
         ffile.write(str(f.dhash)+","+str(f.ihash)+","+f.fname+"\n")
     ffile.close()
 
     ipath = path / "images.bin"
     imgfile = ipath.open(mode="wb+")
-    for  i in imap.values() :
+    for  i in cfg.imap.values() :
         log.info("  Img: %s" , i)
-        imgfile.write(i.ihash.to_bytes(8,'big'))
+        imgfile.write(i.ihash.to_bytes(8,'little'))
         imgfile.write(i.tmb)
     imgfile.close()
 
 
 def load(path:Path):
+   
     dpath =  path / "dirs.txt"
     dirfile = dpath.open() 
     line = dirfile.readline()
-    dtop = Path(line)
+    line = line.rstrip();
+    cfg.dtop = Path(line)
     for line in dirfile:
         line = line.rstrip()
         parts = line.split(",")
-        dentry = dirent(int(parts[0]), parts[1])
-        dlist.append(dentry)
+        dentry = cfg.dirent(int(parts[0]), parts[1])
+        cfg.dlist.append(dentry)
 
     fpath:Path = path / "files.txt"
     filfile = fpath.open()
     for line in filfile:
         line = line.rstrip()
         parts = line.split(",")
-        fentry = filent(int(parts[0]), int(parts[1]), parts[2])
-        flist.append(fentry)
+        fentry = cfg.filent(int(parts[0]), int(parts[1]), parts[2])
+        cfg.flist.append(fentry)
 
     ipath:Path = path / "images.bin"
     imgfile = open(ipath,"rb")
@@ -195,18 +126,25 @@ def load(path:Path):
             lf = struct.calcsize('q')
             r = struct.unpack('q', bcrc)
             crc = r[0]
-            tmb = bytearray(imgfile.read(tnm))
-            img = imgent(crc,tmb)
+            tmb = bytearray(imgfile.read(cfg.tnm))
+            img = cfg.imgent(crc,tmb)
             #print(img)
-            imap[crc] = img
+            cfg.imap[crc] = img
         else:
             break
 
 def printstate():
-    log.warn("Top: %s, #Dirs: %s" , dtop, len(dlist))
-    for  d in dlist :
+
+    log.warn("Top: %s, #Dirs: %s" , cfg.dtop, len(cfg.dlist))
+    for  d in cfg.dlist :
         log.info("  Dir: %s" , d)
-    for  f in flist :
+    for  f in cfg.flist :
         log.info("  File: %s" , f)
-    for  i in imap.values() :
+    for  i in cfg.imap.values() :
         log.info("  Img: %s" , i)
+
+def clear():
+    cfg.dlist.clear()
+    cfg.flist.clear()
+    cfg.imap.clear()
+    
