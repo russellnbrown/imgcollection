0849f0be5bfd6f242af02dcb2a4feac67afb2be7
Russ Brown <50427328+russellnbrown@users.noreply.github.com>
2020-01-15 08:57:16 +1100

    create airport xml reader

---
+++ pplot/Airport.cs
@@ -0,0 +1,21 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Xml;
+
+namespace pplot
+{
+    class Airport
+    {
+        public Airport(string path)
+        {
+            XmlDocument xmlDoc = new XmlDocument();
+            xmlDoc.Load(path);
+            XmlNode titleNode = xmlDoc.SelectSingleNode("//airport/runway");
+            if (titleNode != null)
+                Console.WriteLine(titleNode.InnerText);
+        }
+    }
+}
