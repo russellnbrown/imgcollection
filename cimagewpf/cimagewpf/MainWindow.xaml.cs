3ab06955f42cd8e9a0465ca941215520c4f3331c
Russ Brown <russell.n.brown@gmail.com>
2021-09-12 20:48:55 +1000

    Added core cimagewpf project

---
+++ cimagewpf/cimagewpf/MainWindow.xaml.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Configuration;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cimagewpf
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+            Init();
+        }
+
+        private void Button_Click(object sender, RoutedEventArgs e)
+        {
+
+        }
+
+        private void Init()
+        {
+            TabControl tc = new TabControl();
+            Grid.SetRow(tc, 0);
+            grid.Children.Add(tc);
+
+            Viewer v = new Viewer();
+            TabItem vt = new TabItem();
+            vt.Header = "Viewer";
+            vt.Content = v;
+
+            tc.Items.Add(vt);
+        }
+    }
+}
---
+++ cimagewpf/cimagewpf/Viewer.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace cimagewpf
+{
+    /// <summary>
+    /// Interaction logic for Viewer.xaml
+    /// </summary>
+    public partial class Viewer : UserControl
+    {
+        public Viewer()
+        {
+            InitializeComponent();
+
+
+            Microsoft.Win32.OpenFileDialog openFileDlg = new Microsoft.Win32.OpenFileDialog();
+
+            // Launch OpenFileDialog by calling ShowDialog method
+            Nullable<bool> result = openFileDlg.ShowDialog();
+            // Get the selected file name and display in a TextBox.
+            // Load content of file in a TextBlock
+            if (result == true)
+            {
+
+                string selectedFileName = openFileDlg.FileName;
+
+                BitmapImage bitmap = new BitmapImage();
+                bitmap.BeginInit();
+                bitmap.UriSource = new Uri(selectedFileName);
+                bitmap.EndInit();
+                ImageViewer1.Source = bitmap;
+            }
+        }
+            
+    }
+
+
+}
