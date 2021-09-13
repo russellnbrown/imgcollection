be3da1a8316c57aedc0061ca748d30b3e7618846
russell brown <russell.brown@barrenjoey.com>
2021-09-13 12:09:28 +1000

    drop

---
+++ WpfCoreTester/DirectoryTreeUC.xaml.cs
@@ -0,0 +1,175 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCoreTester
+{
+    /// <summary>
+    /// Interaction logic for DirectoryTreeUC.xaml
+    /// </summary>
+    public partial class DirectoryTreeUC : UserControl
+    {
+        public DirectoryTreeUC()
+        {
+            InitializeComponent();
+            LoadDirectories();
+        }
+
+        public void LoadDirectories()
+        {
+            var drives = DriveInfo.GetDrives();
+            foreach (var drive in drives)
+            {
+                this.treeView.Items.Add(this.GetItem(drive));
+            }
+        }
+
+        private TreeViewItem GetItem(DriveInfo drive)
+        {
+            var item = new TreeViewItem
+            {
+                Header = drive.Name,
+                DataContext = drive,
+                Tag = drive
+            };
+            this.AddDummy(item);
+            item.Expanded += new RoutedEventHandler(item_Expanded);
+            return item;
+        }
+
+        private TreeViewItem GetItem(DirectoryInfo directory)
+        {
+            var item = new TreeViewItem
+            {
+                Header = directory.Name,
+                DataContext = directory,
+                Tag = directory
+            };
+            this.AddDummy(item);
+            item.Expanded += new RoutedEventHandler(item_Expanded);
+            return item;
+        }
+
+        private void AddDummy(TreeViewItem item)
+        {
+            item.Items.Add(new DummyTreeViewItem());
+        }
+
+        private bool HasDummy(TreeViewItem item)
+        {
+            return item.HasItems && (item.Items.OfType<TreeViewItem>().ToList().FindAll(tvi => tvi is DummyTreeViewItem).Count > 0);
+        }
+
+        private void RemoveDummy(TreeViewItem item)
+        {
+            var dummies = item.Items.OfType<TreeViewItem>().ToList().FindAll(tvi => tvi is DummyTreeViewItem);
+            foreach (var dummy in dummies)
+            {
+                item.Items.Remove(dummy);
+            }
+        }
+
+        private TreeViewItem GetItem(FileInfo file)
+        {
+            var item = new TreeViewItem
+            {
+                Header = file.Name,
+                DataContext = file,
+                Tag = file
+            };
+            return item;
+        }
+
+        private void ExploreDirectories(TreeViewItem item)
+        {
+            var directoryInfo = (DirectoryInfo)null;
+            if (item.Tag is DriveInfo)
+            {
+                directoryInfo = ((DriveInfo)item.Tag).RootDirectory;
+            }
+            else if (item.Tag is DirectoryInfo)
+            {
+                directoryInfo = (DirectoryInfo)item.Tag;
+            }
+            else if (item.Tag is FileInfo)
+            {
+                directoryInfo = ((FileInfo)item.Tag).Directory;
+            }
+            if (object.ReferenceEquals(directoryInfo, null)) return;
+            foreach (var directory in directoryInfo.GetDirectories())
+            {
+                var isHidden = (directory.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
+                var isSystem = (directory.Attributes & FileAttributes.System) == FileAttributes.System;
+                if (!isHidden && !isSystem)
+                {
+                    item.Items.Add(this.GetItem(directory));
+                }
+            }
+        }
+
+        private void ExploreFiles(TreeViewItem item)
+        {
+            var directoryInfo = (DirectoryInfo)null;
+            if (item.Tag is DriveInfo)
+            {
+                directoryInfo = ((DriveInfo)item.Tag).RootDirectory;
+            }
+            else if (item.Tag is DirectoryInfo)
+            {
+                directoryInfo = (DirectoryInfo)item.Tag;
+            }
+            else if (item.Tag is FileInfo)
+            {
+                directoryInfo = ((FileInfo)item.Tag).Directory;
+            }
+            if (object.ReferenceEquals(directoryInfo, null)) return;
+            foreach (var file in directoryInfo.GetFiles())
+            {
+                var isHidden = (file.Attributes & FileAttributes.Hidden) == FileAttributes.Hidden;
+                var isSystem = (file.Attributes & FileAttributes.System) == FileAttributes.System;
+                if (!isHidden && !isSystem)
+                {
+                    item.Items.Add(this.GetItem(file));
+                }
+            }
+        }
+
+        void item_Expanded(object sender, RoutedEventArgs e)
+        {
+            var item = (TreeViewItem)sender;
+            if (this.HasDummy(item))
+            {
+                this.Cursor = Cursors.Wait;
+                this.RemoveDummy(item);
+                this.ExploreDirectories(item);
+                this.ExploreFiles(item);
+                this.Cursor = Cursors.Arrow;
+            }
+        }
+
+
+    }
+
+
+    public class DummyTreeViewItem : TreeViewItem
+    {
+        public DummyTreeViewItem()
+            : base()
+        {
+            base.Header = "Dummy";
+            base.Tag = "Dummy";
+        }
+    }
+}
---
+++ WpfCoreTester/MainWindow.xaml.cs
@@ -0,0 +1,48 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace WpfCoreTester
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            InitializeComponent();
+
+            Init();
+
+        }
+
+        private void Init()
+        {
+            TabControl theMainTabControl = new TabControl();
+            
+            Grid.SetRow(theMainTabControl, 0);
+            mainGrid.Children.Add(theMainTabControl);
+
+            TabItem explorerTabItem = new TabItem();
+            explorerTabItem.Header = "Explorer";
+            ExplorerTab explorerTab = new ExplorerTab();
+            explorerTabItem.Content = explorerTab;
+            theMainTabControl.Items.Add(explorerTabItem);
+        }
+    }
+
+}
+
