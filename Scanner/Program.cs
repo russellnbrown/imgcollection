3f37aff00db94b125a2b15e0bf392b02351a1b88
Russ Brown <russell.n.brown@gmail.com>
2021-09-20 13:58:17 +1000

    drop

--- Scanner/Program.cs
+++ Scanner/Program.cs
@@ -17,7 +17,7 @@ namespace Scanner
             Application.SetHighDpiMode(HighDpiMode.SystemAware);
             Application.EnableVisualStyles();
             Application.SetCompatibleTextRenderingDefault(false);
-            Application.Run(new Form1());
+            Application.Run(new InitialSelection());
         }
     }
 }
