db0661a83109277b78a5466f848f3eed47f50069
russell brown <russell.brown@barrenjoey.com>
2021-09-29 14:40:41 +1000

    Better explorer

--- WpfCoreTester/ExplorerTree.xaml.cs
+++ WpfCoreTester/ExplorerTree.xaml.cs
@@ -21,6 +21,9 @@ namespace WpfCoreTester
     /// </summary>
     public partial class ExplorerTree : UserControl
     {
+        private object dummyNode = null;
+        public string SelectedImagePath { get; set; }
+
         public ExplorerTree()
         {
             InitializeComponent();
@@ -39,5 +42,60 @@ namespace WpfCoreTester
                 foldersItem.Items.Add(item);
             }
         }
+
+
+        void folder_Expanded(object sender, RoutedEventArgs e)
+        {
+            TreeViewItem item = (TreeViewItem)sender;
+            if (item.Items.Count == 1 && item.Items[0] == dummyNode)
+            {
+                item.Items.Clear();
+                try
+                {
+                    foreach (string s in Directory.GetDirectories(item.Tag.ToString()))
+                    {
+                        TreeViewItem subitem = new TreeViewItem();
+                        subitem.Header = s.Substring(s.LastIndexOf("\\") + 1);
+                        subitem.Tag = s;
+                        subitem.FontWeight = FontWeights.Normal;
+                        subitem.Items.Add(dummyNode);
+                        subitem.Expanded += new RoutedEventHandler(folder_Expanded);
+                        item.Items.Add(subitem);
+                    }
+                }
+                catch (Exception) { }
+            }
+        }
+
+        private void foldersItem_SelectedItemChanged(object sender, RoutedPropertyChangedEventArgs<object> e)
+        {
+            TreeView tree = (TreeView)sender;
+            TreeViewItem temp = ((TreeViewItem)tree.SelectedItem);
+
+            if (temp == null)
+                return;
+            SelectedImagePath = "";
+            string temp1 = "";
+            string temp2 = "";
+            while (true)
+            {
+                temp1 = temp.Header.ToString();
+                if (temp1.Contains(@"\"))
+                {
+                    temp2 = "";
+                }
+                SelectedImagePath = temp1 + temp2 + SelectedImagePath;
+                if (temp.Parent.GetType().Equals(typeof(TreeView)))
+                {
+                    break;
+                }
+                temp = ((TreeViewItem)temp.Parent);
+                temp2 = @"\";
+            }
+            //show user selected path
+            MessageBox.Show(SelectedImagePath);
+        }
+
     }
 }
+
