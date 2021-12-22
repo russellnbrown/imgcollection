1d7ebc33b5268b4cf8e7d69a9e894098b229c486
russell brown <russell.brown@barrenjoey.com>
2021-12-22 12:08:34 +1100

    drop

--- jsync/src/jsync/Jsync.java
+++ jsync/src/jsync/Jsync.java
@@ -5,24 +5,76 @@
  */
 package jsync;
 
-/**
- *
- * @author russell.brown
- */
-public class Jsync {
 
+import java.io.IOException;
+import java.nio.file.*;
+import java.nio.file.attribute.BasicFileAttributes;
+
+
+ 
+public class Jsync implements FileVisitor<Path> {
+
+    
+    private long dcount = 0;
+    private long fcount = 0;
+    private long ecount = 0;
+    
     /**
      * @param args the command line arguments
      */
     public static void main(String[] args) 
     {
         if ( args.length != 3 )
+            NLog.f("usage: <src> <dst> <test|sync>");
+        
+        new Jsync(Paths.get(args[0]), Paths.get(args[1]), args[2]);
+        
+    }
+
+
+    
+    private Jsync(Path src, Path dest, String op) 
+    {
+        NLog.i("Syncing " + src.toString() + " to " + dest.toString() + " using " + op);
+        try
         {
-            System.out.println("usage: <src> <dst> <test|sync>");
-            System.exit(-1);
+            Files.walkFileTree(src, this);
+        }
+        catch(Exception e)
+        {
+            
         }
         
+        NLog.i("Visited dirs=", dcount , ", files=", fcount, ", errors=", ecount);
         
+    }
+
+    @Override
+    public FileVisitResult preVisitDirectory(Path dir, BasicFileAttributes attrs) throws IOException 
+    {
+        //NLog.i("Pre ", dir.toAbsolutePath());
+        dcount++;
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFile(Path file, BasicFileAttributes attrs) throws IOException {
+        //NLog.i("At ", file.toAbsolutePath());
+        fcount++;
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult visitFileFailed(Path file, IOException exc) throws IOException {
+        NLog.i("Err ", file.toAbsolutePath());
+        ecount++;
+        return FileVisitResult.CONTINUE;
+    }
+
+    @Override
+    public FileVisitResult postVisitDirectory(Path dir, IOException exc) throws IOException {
+        //NLog.i("Post ", dir.toAbsolutePath());
+        return FileVisitResult.CONTINUE;
     }
     
 }
